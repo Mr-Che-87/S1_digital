245c8c7cb85d7f11936e0ef953f5c398ba0f2d89
Alexey Chechetkin <109634077+Mr-Che-87@users.noreply.github.com>
2024-08-11 17:20:43 +0300

    1008

--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -54,7 +54,7 @@ const BlogPage = () => {
               strokeLinejoin="round"
             />
           </svg>
-          <HistoryLink to={"/blogs"}>блоги</HistoryLink>
+          <HistoryLink to={"/blogs"}>Блоги</HistoryLink>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="6"
